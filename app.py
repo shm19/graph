a52e3d2738090d7645fa7009c23b014290548ed9
shm <malekishayan79@gmail.com>
2023-01-21 10:42:17 +0330

    feat: change implementation for least node

--- app.py
+++ app.py
@@ -7,8 +7,8 @@ G = nx.read_edgelist('graph.txt', nodetype=int, data=(('weight', float),))
 all_connected_subgraphs = []
 
 # here we ask for all connected subgraphs that have at least 2 nodes AND have less nodes than the input graph
-for nb_nodes in range(2, G.number_of_nodes()):
-    for SG in (G.subgraph(selected_nodes) for selected_nodes in itertools.combinations(G, nb_nodes)):
+for nb_nodes in range(1, G.number_of_nodes()):
+    for SG in (G.subgraph(selected_nodes).to_undirected() for selected_nodes in itertools.combinations(G, nb_nodes)):
         if nx.is_connected(SG):
             print(SG.nodes)
             with open('subgraphs.txt', 'a') as f:
