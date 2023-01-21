d2fd7a1ccdd1d517aa5709c0c301ebe2a6aab7a2
shm <malekishayan79@gmail.com>
2023-01-21 10:59:45 +0330

    add not connected graphs

--- app.py
+++ app.py
@@ -11,7 +11,6 @@ all_connected_subgraphs = []
 # here we ask for all connected subgraphs that have at least 2 nodes AND have less nodes than the input graph
 for nb_nodes in range(1, G.number_of_nodes()+1):
     for SG in (G.subgraph(selected_nodes).to_undirected() for selected_nodes in itertools.combinations(G, nb_nodes)):
-        if nx.is_connected(SG):
             print(SG.nodes)
             with open('subgraphs.txt', 'a') as f:
                 f.write(str(SG.nodes) + '\n')
